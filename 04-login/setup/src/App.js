93cf2c379c6cf26ff2da75af3169779f616b9c65
sboy99 <shadowboy191@gmail.com>
2022-06-25 23:13:01 +0530

    login added

---
+++ 04-login/setup/src/App.js
@@ -0,0 +1,15 @@
+import "./App.scss";
+import React, { useState } from "react";
+import { Form } from "./Form";
+
+function App() {
+  return (
+    <main>
+      <section className="main-container ">
+        <Form />
+      </section>
+    </main>
+  );
+}
+
+export default App;
