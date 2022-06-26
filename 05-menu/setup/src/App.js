e9ab6f45c6af30b4057cc594bd8e94188219c11e
sboy99 <shadowboy191@gmail.com>
2022-06-26 22:28:25 +0530

    complete

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -1,10 +1,38 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([
+    "all",
+    "breakfast",
+    "lunch",
+    "shakes",
+  ]);
+
+  //filter by category
+  const filterItems = (category) => {
+    if (category === "all") {
+      return setMenuItems(items);
+    }
+    const newItem = items.filter((item) => item.category === category);
+    setMenuItems(newItem);
+  };
+
+  return (
+    <main>
+      <section className="menu  section">
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filter={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- 05-menu/setup/src/Categories.js
+++ 05-menu/setup/src/Categories.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React from "react";
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({ categories, filter }) => {
+  const active = (id) => {
+    document.querySelectorAll(".filter-btn").forEach((item) => {
+      item.classList.remove("active");
+    });
+    document.querySelectorAll(".filter-btn")[id].classList.add("active");
+  };
+  return (
+    <>
+      <div className="btn-container">
+        {categories.map((category, i) => {
+          return (
+            <button
+              className={
+                category === "all" ? "filter-btn active" : "filter-btn"
+              }
+              id={i}
+              onClick={() => {
+                filter(category);
+                active(i);
+              }}
+            >
+              {category}
+            </button>
+          );
+        })}
+      </div>
+    </>
+  );
 };
 
 export default Categories;
